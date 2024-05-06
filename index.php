405593b8a7f5ca9b5b9114c7ab124fd81828a0a6
ahucha <ahuchard91@gmail.com>
2024-05-06 20:03:27 +0200

    index modifié

--- index.php
+++ index.php
@@ -3,38 +3,35 @@ session_start();
 
 include 'view/header.php';
 
-// Vérifier si une action spécifique est demandée pour l'utilisateur
-if(isset($_GET['ctl']) && $_GET['ctl'] == 'user') {
-    // Inclure le contrôleur utilisateur
-    include 'controller/ctl_user.php';
-} 
-else 
+// Inclure la navbar si l'utilisateur est connecté
+if (isset($_SESSION["id_user"]))
 {
-    // Vérifier si l'utilisateur est connecté
-    if (!isset($_SESSION["id_user"])) 
-    {
-        // Si l'utilisateur n'est pas connecté, inclure la page de connexion
-        include "view/view_user/login.php";
-    } 
-    else
-    {
-        // Si l'utilisateur est connecté, inclure la navbar
-        include 'view/navbar.php';
+    include 'view/navbar.php';
+}
 
-        // Vérifier s'il y a une action spécifique demandée
-        if(isset($_GET['ctl'])) {
-            switch($_GET['ctl']) {
-                case 'annonce':
-                    // Inclure le contrôleur des annonces
-                    include 'controller/ctl_annonce.php';
-                    break;
-                // Ajoutez d'autres cas si nécessaire
-                default:
-                    // Inclure un contrôleur par défaut
-                    include 'controller/ctl_annonce.php';
-                    break;
-            }
-        }
+// Vérifier si une action spécifique est demandée
+if(isset($_GET['ctl'])) 
+{
+    switch($_GET['ctl']) 
+    {
+        case 'user':
+            include 'controller/ctl_user.php';
+            break;
+        case 'annonce':
+            include 'controller/ctl_annonce.php';
+            break;
+        // Ajoutez d'autres cas si nécessaire
+        default:
+            include 'controller/ctl_annonce.php';
+            break;
+    }
+}
+else
+{
+    // Si aucun contrôleur n'est spécifié, inclure la page de connexion si l'utilisateur n'est pas connecté
+    if (!isset($_SESSION["id_user"]))
+    {
+        include "view/view_user/login.php";
     }
 }
 
--- view/view_user/profil_user.php
+++ view/view_user/profil_user.php
@@ -1,11 +1,11 @@
+<h1>VUE PROFIL</h1>
 <?php
 // Assurez-vous que $infoUser est défini avant de l'utiliser
 if(isset($infoUser)) {
-    foreach($infoUser as $user) {
+    foreach($infoUser as $user) 
+    {
         echo "username: " . $user['username'] . "<br>";
         echo "email: " . $user['email'] . "<br>";
     }
-} else {
-    echo "Aucune information sur l'utilisateur.";
 }
 ?>
